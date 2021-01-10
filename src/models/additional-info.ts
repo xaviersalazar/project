ec4150bb8dd3fd29e2156a7fb21ca87ceb896323
Xavier Salazar <salazar.xavier26@gmail.com>
2021-01-10 10:57:22 -0600

    adding models

---
+++ src/models/additional-info.ts
@@ -0,0 +1,6 @@
+export interface AdditionalInfo {
+  company: string;
+  email: string;
+  phone: string;
+  address: string;
+}
---
+++ src/models/info.ts
@@ -0,0 +1,8 @@
+import { AdditionalInfo } from "./additional-info";
+
+export interface Info {
+  age: number;
+  name: string;
+  gender: string;
+  additionalInfo: AdditionalInfo;
+}
