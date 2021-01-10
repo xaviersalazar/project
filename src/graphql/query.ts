016b5273b2f6ff371dd69fef1d10af2fa64dc13b
Xavier Salazar <salazar.xavier26@gmail.com>
2021-01-10 16:45:48 -0600

    adding query for single client and additionalInfo only

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,18 +1,38 @@
-import { GraphQLList, GraphQLObjectType } from "graphql";
+import { GraphQLID, GraphQLList, GraphQLObjectType } from "graphql";
 import fs from "fs";
-import { ClientType } from "./type";
+import { AdditionalInfoType, ClientType } from "./type";
 import { Client } from "src/models/info";
 
 const data = fs.readFileSync(__dirname + "/../../sampleData.json", "utf-8");
 
+const parseData = () => {
+  return JSON.parse(data).clients as Client[];
+};
+
 export const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve: (source, args) => {
+        const clients = parseData();
+        return clients.find((client) => client.id === args.id);
+      },
+    },
     clients: {
       type: new GraphQLList(ClientType),
       resolve: () => {
-        const clients: Client[] = JSON.parse(data).clients;
+        const clients = parseData();
         return clients;
+      },
+    },
+    additionalInfo: {
+      type: AdditionalInfoType,
+      args: { id: { type: GraphQLID } },
+      resolve: (source, args) => {
+        const clients = parseData();
+        return clients.find((client) => client.id === args.id)?.additionalInfo;
       },
     },
   },
