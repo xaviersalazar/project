abfe1957cf250954b850ebf329696a423fa3bd06
Xavier Salazar <salazar.xavier26@gmail.com>
2021-01-10 17:03:17 -0600

    adding client types

---
+++ client/src/types/Client.ts
@@ -0,0 +1,19 @@
+export interface IClient {
+  id: string;
+  age: number;
+  name: string;
+  gender: string;
+  additionalInfo: IAdditionalInfo;
+}
+
+export interface IAdditionalInfo {
+  gender: string;
+  company: string;
+  email: string;
+  phone: string;
+  address: string;
+}
+
+export interface IClients {
+  getClients: IClient[];
+}
