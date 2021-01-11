fc1d132ad5d1fb257c973d9341f18b4a33d133fe
Xavier Salazar <salazar.xavier26@gmail.com>
2021-01-11 11:50:35 -0600

    removing graphql playground

--- index.ts
+++ index.ts
@@ -6,10 +6,6 @@ import schema from "./src/graphql/schema";
 const app = express();
 const PORT = 5000;
 
-// Remove later
-const expressPlayground = require("graphql-playground-middleware-express")
-  .default;
-
 app.use(cors());
 
 app.use(
@@ -19,9 +15,6 @@ app.use(
     graphiql: true,
   })
 );
-
-// Remove later
-app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
 
 app.listen(PORT, () => {
   console.log(`⚡️ Server is running at http://localhost:${PORT}`);
