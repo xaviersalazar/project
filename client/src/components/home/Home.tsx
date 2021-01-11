f10102b5e5182acb8607a4ba7affd792772bc4d8
Xavier Salazar <salazar.xavier26@gmail.com>
2021-01-10 23:15:14 -0600

    styling home

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_CLIENTS } from "../../graphql";
 import { IClient } from "../../types/Client";
-import { Link } from "react-router-dom";
+import "./Home.css";
 
 export const Home: React.FC = () => {
   const { loading, error, data } = useQuery(GET_CLIENTS);
@@ -11,12 +12,28 @@ export const Home: React.FC = () => {
   if (error) return <h1>Oops, something went wrong</h1>;
 
   return (
-    <div>
-      {data.clients?.map((client: IClient) => (
-        <Link to={`/${client.id}`} key={client.id}>
-          <h1>Client Name: {client.name}</h1>
-        </Link>
-      ))}
+    <div className="container">
+      <h1 className="title">Clients</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.clients?.map((client: IClient) => (
+            <tr key={client.id}>
+              <td>
+                <Link to={`/${client.id}`}>{client.id}</Link>
+              </td>
+              <td>{client.name}</td>
+              <td>{client.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
