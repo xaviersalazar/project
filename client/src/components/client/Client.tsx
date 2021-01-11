bc4a6cb9d084626206b9c60497c1fcba27be686f
Xavier Salazar <salazar.xavier26@gmail.com>
2021-01-11 11:09:15 -0600

    adding loader component

--- client/src/components/client/Client.tsx
+++ client/src/components/client/Client.tsx
@@ -3,33 +3,36 @@ import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { GET_CLIENT } from "../../graphql";
 import { ClientDetails } from "./ClientDetails";
+import { Loader } from "../loader/Loader";
 import "./Client.css";
 
 export const Client: React.FC = () => {
   const { id } = useParams<{ id: string }>();
-  const { loading, error, data } = useQuery(GET_CLIENT, {
+  const { loading, data } = useQuery(GET_CLIENT, {
     variables: { id: id },
   });
 
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <h1>Oops, something went wrong</h1>;
-  console.log("data: ", data);
-
   return (
     <div className="client-card">
-      <div className="client-pic">
-        <div className="pic" />
-        <p className="pic-name">
-          {data.client?.name
-            .split(" ")
-            .map((n: string) => n[0])
-            .join("")}
-        </p>
-        <h1>{data.client?.name}</h1>
-      </div>
-      {Object.entries(data.client?.additionalInfo).map(
-        ([k, v]) =>
-          k !== "__typename" && <ClientDetails key={k} label={k} info={v} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <React.Fragment>
+          <div className="client-pic">
+            <div className="pic" />
+            <p className="pic-name">
+              {data.client?.name
+                .split(" ")
+                .map((n: string) => n[0])
+                .join("")}
+            </p>
+            <h1>{data.client?.name}</h1>
+          </div>
+          {Object.entries(data.client?.additionalInfo).map(
+            ([k, v]) =>
+              k !== "__typename" && <ClientDetails key={k} label={k} info={v} />
+          )}
+        </React.Fragment>
       )}
     </div>
   );
--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -3,37 +3,39 @@ import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_CLIENTS } from "../../graphql";
 import { IClient } from "../../types/Client";
+import { Loader } from "../loader/Loader";
 import "./Home.css";
 
 export const Home: React.FC = () => {
-  const { loading, error, data } = useQuery(GET_CLIENTS);
-
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <h1>Oops, something went wrong</h1>;
+  const { loading, data } = useQuery(GET_CLIENTS);
 
   return (
     <div className="container">
       <h1 className="title">Clients</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Age</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.clients?.map((client: IClient) => (
-            <tr key={client.id}>
-              <td>
-                <Link to={`/${client.id}`}>{client.id}</Link>
-              </td>
-              <td>{client.name}</td>
-              <td>{client.age}</td>
+      {loading ? (
+        <Loader />
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Age</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data.clients?.map((client: IClient) => (
+              <tr key={client.id}>
+                <td>
+                  <Link to={`/${client.id}`}>{client.id}</Link>
+                </td>
+                <td>{client.name}</td>
+                <td>{client.age}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
